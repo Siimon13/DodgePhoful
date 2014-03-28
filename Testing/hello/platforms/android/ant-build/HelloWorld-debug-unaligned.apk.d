05f635007ab633154de8f380d85803180a6ebab0
Simon <chensimon@outlook.com>
2014-03-28 02:38:12 -0400

    SDK Installed wrong. Stuff happened. Will make a live track gps.

---
+++ Testing/hello/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/HelloWorld.ap_ \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes.dex \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/CordovaLib/ant-build/classes.jar \
---
+++ Testing/hello/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes.dex : \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/dexedLibs/classes-263e189de57bfbb60c8abb57055151f5.jar \
