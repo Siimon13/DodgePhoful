1a3b550f063001b06b95cf4faf2b6b056773738c
Simon <chensimon@outlook.com>
2014-04-06 12:36:34 -0400

    Changing files to allow for GPS

--- Testing/geo.js
+++ Testing/geo.js
@@ -1,54 +1,16 @@
-var watchID = null;
+
 $(document).ready(function(){
-    var optn = {
-	enableHighAccuracy: true,
-	timeout: Infinity,
-	maximumAge: 0
-    };
-    if (navigator.geolocation)
-	navigator.geolocation.watchPosition(success, null, optn);
-    else
-	$("p").html("HTML5 NOT SUPPORTED");
+    navigator.geolocation.watchPosition(getLoc);
 });
 
-function success(position){
-    var googleLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var map0tn={
-	zoom:10,
-	center:googleLatLng,
-	mapTypeId:google.maps.MapTypeId.ROAD
-    };
+// For the radar
+// window.setInterval(function(){
+//     navigator.geolocation.watchPosition(getLoc);
+// }, 500);
 
-    var Pmap = document.getElementById("map");
-
-    var map = new google.maps.Map(Pmap,map0tn);
-    addMarker(map,googleLatLng, "Here");
-
-    function addMarker(map, googleLatLng, content){
-	var markerOptn={
-	    position:googleLatLng,
-	    map:map,
-	    animation:google.maps.Animation.DROP
-	};
-
-	var marker = new google.maps.Marker(markerOptn);
-    }
-
-    var infoWindow = new google.maps.InfoWindow({
-	content: content,
-	position: googleLatLng
-    });
-
-    google.maps.event.addListener(marker, "click", function(){
-	infoWindow.open(map);
-    });
-
-    function fail(error){
-	var errorType={
-	    0:"Unknown Error",
-	    1:"Permission denied by the user",
-	    2:"Position of the user not available",
-	    3:"Request timed out"
-	};
-    }
+    
+function getLoc(position){
+    console.log("updating")
+    document.getElementById('latitude').innerHTML = position.coords.latitude;
+    document.getElementById('longitude').innerHTML = position.coords.longitude;
 }
