b9319fa9f8567cd19f17d2391b0b7bd20f352b79
Simon <chensimon@outlook.com>
2014-04-10 13:07:28 -0400

    Fixing merge problems

--- Testing/hello/platforms/android/ant-build/DodgePhoful-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/DodgePhoful-debug-unaligned.apk : \
-/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/DodgePhoful.ap_ \
-/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes.dex \
-/home/simon/DodgePhoful/Testing/hello/platforms/android/CordovaLib/ant-build/classes.jar \
