03ae9d628601e383836b30560d4d46049851488c
Simon <chensimon@outlook.com>
2014-04-10 13:05:39 -0400

    Merg'd Branches

---
+++ Testing/hello/platforms/android/ant-build/DodgePhoful-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/DodgePhoful-debug-unaligned.apk : \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/DodgePhoful.ap_ \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/ant-build/classes.dex \
+/home/simon/DodgePhoful/Testing/hello/platforms/android/CordovaLib/ant-build/classes.jar \
